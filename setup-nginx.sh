9a27324885b2d8b9a6bb60b10e720a2bb54cfb9f
Milan Gruner <milangruner@gmail.com>
2024-01-01 14:19:45 +0100

    Add Let's Encrypt cert commands
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -7,4 +7,10 @@ while read -r LINE; do
 done < .env
 envsubst '$SERVER_NAME' < nginx.conf.template > nginx.conf
 
+# see here: https://phoenixnap.com/kb/letsencrypt-docker
+
+# first setup with certbot
 # docker-compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d $SERVER_NAME
+
+# refresh certificate
+# docker-compose run --rm certbot renew
