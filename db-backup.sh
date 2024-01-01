8033e841c5b5816b5a33e68370a30c8b60fc39ea
Milan Gruner <milangruner@gmail.com>
2024-01-01 12:19:17 +0100

    Write compressed database backups
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- db-backup.sh
+++ db-backup.sh
@@ -3,9 +3,11 @@
 
 now=$(date +"%d-%m-%Y_%H-%M")
 export PGPASSWORD="metabase"
-pg_dump -h db -U metabase -d metabase -w > "/db_backups/db_dump_$now.sql"
+# Uncomment this if you prefer uncompressed/ raw SQL backups
+# pg_dump -h db -U metabase -d metabase -w > "/db_backups/db_dump_$now.sql"
+pg_dump -Fc -h db -U metabase -d metabase -w > "/db_backups/db_dump_$now.dump"
 
 # remove all files (type f) modified longer than 30 days ago under /db_backups/backups
-find /db_backups -name "*.sql" -type f -mtime +30 -delete
+find /db_backups -name "*.sql" -o -name "*.dump" -type f -mtime +30 -delete
 
 exit 0
