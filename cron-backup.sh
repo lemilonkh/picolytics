287e3972fa0ffdc24be3c6ab6b6aa30b730ebdf6
Milan Gruner <milangruner@gmail.com>
2024-01-01 12:19:50 +0100

    Simplify command names for cron-backup.sh
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- cron-backup.sh
+++ cron-backup.sh
@@ -6,14 +6,14 @@ cronjob_backup="00 22 * * * $croncmd_backup"
 
 if [[ $# -eq 0 ]] ; then
     echo -e 'Please provide one of the arguments (example: ./cron-backup.sh add-cron-db-backup):
-    1 > add-cron-db-backup
-    2 > remove-cron-db-backup'
+    1 > add
+    2 > remove'
 
-elif [[ $1 == add-cron-db-backup ]]; then
+elif [[ $1 == add ]]; then
     ( crontab -l | grep -v -F "$croncmd_backup" ; echo "$cronjob_backup" ) | crontab -
     echo "==>>> Backup task added to Local (not container) Cron"
 
-elif [[ $1 == remove-cron-db-backup ]]; then
+elif [[ $1 == remove ]]; then
     ( crontab -l | grep -v -F "$croncmd_backup" ) | crontab -
     echo "==>>> Backup task removed from Cron"
 fi
